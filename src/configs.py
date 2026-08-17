49d55ff8588c51944e649fdf79fc4cdcad091b40
Nikhil Singh <nikhils1220@gmail.com>
2026-08-17 06:46:40 -0700

    Updated configs

--- src/configs.py
+++ src/configs.py
@@ -1,3 +1,40 @@
 # width and height of image
 IMAGE_WIDTH = 224
 IMAGE_HEIGHT = 224
+
+# seed for reproducibility
+SEED = 7
+
+# number of classes
+C = 20
+
+CLASS_TO_IDX = {
+    "aeroplane": 0,
+    "bicycle": 1,
+    "bird": 2,
+    "boat": 3,
+    "bottle": 4,
+    "bus": 5,
+    "car": 6,
+    "cat": 7,
+    "chair": 8,
+    "cow": 9,
+    "diningtable": 10,
+    "dog": 11,
+    "horse": 12,
+    "motorbike": 13,
+    "person": 14,
+    "pottedplant": 15,
+    "sheep": 16,
+    "sofa": 17,
+    "train": 18,
+    "tvmonitor": 19,
+}
+
+IDX_TO_CLASS = {
+    idx: class_
+    for class_, idx in CLASS_TO_IDX.items()
+}
+
+# size of batches in DataLoader
+BATCH_SIZE = 32
