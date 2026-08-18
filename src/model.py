fb97bd22218b65414ffac9f16a3d35bfcced244e
Nikhil Singh <nikhils1220@gmail.com>
2026-08-18 05:28:20 -0700

    Updated model

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 from torchvision.models import resnet18
+from configs import D
 import torch.nn as nn
 import torch
 
@@ -12,8 +13,11 @@ class Model(nn.Module):
             *list(resnet18(weights="DEFAULT").children())[:-2]
         )
 
+        # project channels to configured embedded dimensions
+        self.projection = nn.Conv2d(512, D, kernel_size=1)
+
     def forward(self, x):
-        x = self.backbone(x)
+        x = self.projection(self.backbone(x))
         return x
 
 inputs = torch.randn(32, 3, 224, 224)
