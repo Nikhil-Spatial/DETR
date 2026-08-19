0597748c6725c38aa315e9d7d413e606bf74014d
Nikhil Singh <nikhils1220@gmail.com>
2026-08-19 10:09:07 -0700

    Added decoder layers config

--- src/configs.py
+++ src/configs.py
@@ -59,5 +59,8 @@ BB_CHANNELS = 512
 # number of layers in transformer encoder
 ENCODER_LAYERS = 6
 
+# number of layers in transformer decoder
+DECODER_LAYERS = 6
+
 # number of object queries/prediction slots
 N = 100
