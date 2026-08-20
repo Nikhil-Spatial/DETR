d6d0ee2adf63e941412259cc2f4f02fdf27e3d95
Nikhil Singh <nikhils1220@gmail.com>
2026-08-20 04:56:16 -0700

    Finished model architecture, time for hungarian matching and loss

--- src/bbox_regression_head.py
+++ src/bbox_regression_head.py
@@ -1,4 +1,5 @@
 import torch.nn.functional as F
+from configs import D
 from torch import nn
 import torch
 
@@ -6,9 +7,16 @@ class bbox_regression_FFN(nn.Module):
     def __init__(self):
         super().__init__()
 
+        # the two hidden layers
         self.affine_transform_1 = nn.Linear(D, D)
         self.affine_transform_2 = nn.Linear(D, D)
 
         # the output dimension of 4 represents the 4 predictions:
         # (center_x, center_y, width, height)
         self.output_layer = nn.Linear(D, 4)
+
+    def forward(self, x):
+        x = F.relu(self.affine_transform_1(x))
+        x = F.relu(self.affine_transform_2(x))
+
+        return F.sigmoid(self.output_layer(x))
--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 from torchvision.models import resnet18
 from transformer_encoder import positional_encoding, TransformerEncoder
 from transformer_decoder import TransformerDecoder
+from bbox_regression_head import bbox_regression_FFN
 from configs import D, H, W, BB_CHANNELS, N, C
 from torch import nn
 import torch
@@ -36,6 +37,11 @@ class Model(nn.Module):
         # the + 1 represents a "no-object" class
         self.class_head = nn.Linear(D, C+1)
 
+        # bounding box regression head: refines predictions, and then projects
+        # D embedded dims to 4 dims for the center coordinates, height, and
+        # width.
+        self.bbox_regression_head = bbox_regression_FFN()
+
     def forward(self, x):
         batch_size = x.shape[0]
 
@@ -53,10 +59,13 @@ class Model(nn.Module):
         # encodings/embeddings to the decoder
         x = self.transformer_decoder(self.query_pos, x, self.pos_encoding)
 
+        # classification and bounding box regression prediction heads
         x_class = self.class_head(x)
+        x_bbox_pred = self.bbox_regression_head(x)
 
-        return x_class
+        return x_class, x_bbox_pred
 
 inputs = torch.randn(32, 3, 224, 224)
 model = Model()
-print(model(inputs).shape)
+class_pred, bbox_pred = model(inputs)
+print(class_pred.shape, bbox_pred.shape)
