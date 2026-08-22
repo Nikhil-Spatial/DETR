9d9596ed156656c4d40e1208b51035379e62402d
Nikhil Singh <nikhils1220@gmail.com>
2026-08-22 06:23:29 -0700

    Finished GIoU function after learning broadcasting

--- src/utilities.py
+++ src/utilities.py
@@ -2,26 +2,6 @@ from torch import round as rd
 from src.configs import IMAGE_WIDTH, IMAGE_HEIGHT
 import torch
 
-def compute_intersection_coords(bbox_1, bbox_2):
-    intersect_boxes = torch.empty(bbox_1.shape[0], 4)
-
-    intersect_boxes[..., 0] = torch.max(bbox_1[..., 0], bbox_2[0])
-    intersect_boxes[..., 1] = torch.max(bbox_1[..., 1], bbox_2[1])
-    intersect_boxes[..., 2] = torch.min(bbox_1[..., 2], bbox_2[2])
-    intersect_boxes[..., 3] = torch.min(bbox_1[..., 3], bbox_2[3])
-
-    return intersect_boxes
-
-def compute_enclosed_coords(bbox_1, bbox_2):
-    enclosed_boxes = torch.empty(bbox_1.shape[0], 4)
-
-    enclosed_boxes[..., 0] = torch.min(bbox_1[..., 0], bbox_2[0])
-    enclosed_boxes[..., 1] = torch.min(bbox_1[..., 1], bbox_2[1])
-    enclosed_boxes[..., 2] = torch.max(bbox_1[..., 2], bbox_2[2])
-    enclosed_boxes[..., 3] = torch.max(bbox_1[..., 3], bbox_2[3])
-
-    return enclosed_boxes
-
 def cxcywh_to_xyxy(bboxes, draw=False):
     # 1) unnormalize (center_x, center_y, width, height)
     bboxes[..., [0, 2]] *= IMAGE_WIDTH
@@ -44,38 +24,45 @@ def cxcywh_to_xyxy(bboxes, draw=False):
     return xyxy
 
 def compute_area(bbox):
-    # compute area for one bbox or a tensor full of them
-    w = torch.clamp(bbox[..., 2] - bbox[..., 0], min=0)
-    h = torch.clamp(bbox[..., 3] - bbox[..., 1], min=0)
+    w = (bbox[..., 2] - bbox[..., 0]).clamp(min=0)
+    h = (bbox[..., 3] - bbox[..., 1]).clamp(min=0)
 
     return w * h
 
 def compute_giou(bbox_1, bbox_2):
-    giou_costs = []
+    # 1) find intersection coords, and compute area of intersection box
+    inter_x1 = torch.max(bbox_1[:, :, None, 0], bbox_2[:, None, :, 0])
+    inter_y1 = torch.max(bbox_1[:, :, None, 1], bbox_2[:, None, :, 1])
+    inter_x2 = torch.min(bbox_1[:, :, None, 2], bbox_2[:, None, :, 2])
+    inter_y2 = torch.min(bbox_1[:, :, None, 3], bbox_2[:, None, :, 3])
 
-    for i in range(bbox_2.shape[0]):
-        # 1) find coordinates of box that encloses both boxes, and compute its
-        # area
-        rectangle_coords = compute_enclosed_coords(bbox_1, bbox_2[i])
-        rectangle_area = compute_area(rectangle_coords)
+    intersection_coords = torch.cat([
+        inter_x1[:, :, :, None],
+        inter_y1[:, :, :, None],
+        inter_x2[:, :, :, None],
+        inter_y2[:, :, :, None]
+    ], dim=-1)
 
-        # 2) find coordinates of box that intersects both boxes, and compute
-        # its area
-        intersection_coords = compute_intersection_coords(
-            bbox_1, bbox_2[i]
-        )
-        intersection_area = compute_area(intersection_coords)
+    intersection = compute_area(intersection_coords)
 
-        # 3) compute union
-        union_area = (compute_area(bbox_1) + compute_area(bbox_2[i])
-                      - compute_area(intersection_coords))
+    # 2) compute union
+    union_ = compute_area(bbox_1) + compute_area(bbox_2) - intersection
 
-        # 4) compute IoU
-        iou = intersection_area / union_area
+    # 3) compute area of rectangle that encloses both bboxes
+    rect_x1 = torch.min(bbox_1[:, :, None, 0], bbox_2[:, None, :, 0])
+    rect_y1 = torch.min(bbox_1[:, :, None, 1], bbox_2[:, None, :, 1])
+    rect_x2 = torch.max(bbox_1[:, :, None, 2], bbox_2[:, None, :, 2])
+    rect_y2 = torch.max(bbox_1[:, :, None, 3], bbox_2[:, None, :, 3])
 
-        # 5) compute GIoU
-        giou_costs.append(
-            iou - ((rectangle_area - union_area) / rectangle_area)
-        )
+    rectangle_coords = torch.cat([
+        rect_x1[:, :, :, None],
+        rect_y1[:, :, :, None],
+        rect_x2[:, :, :, None],
+        rect_y2[:, :, :, None]
+    ], dim=-1)
 
-    return torch.cat(giou_costs, dim=-1)
+    rectangle_area = compute_area(rectangle_coords)
+
+    # compute GIoU and return
+    return (intersection / union_) - ((rectangle_area - union_) /
+                                      rectangle_area)
